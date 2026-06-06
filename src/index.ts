f86d8bea3003cc8d3440dfd48ba40bb57375152b
rishabh392004 <rishabhguptkd@gmail.com>
2026-06-06 16:50:38 +0530

    Graphql server is setup completest

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const app=express();
+const server = new ApolloServer({
+    //typedef pe hm schema dete haiii
+  typeDefs: `#graphql
+    type Query {
+      hello: String
+      say(name: String!): String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "Hello world!",
+      say: (_,{name})=> `Hello ${name},How are you`
+    },
+  },
+});
+const PORT=Number(process.env.PORT)||8000;
+app.get('/',(req ,res) => {
+res.json({message: "Server is Running"})
+})
+//start graphql server
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: PORT },
+});
+
+console.log(`GraphQL Server completely ready at: ${url}`);
